6c99c5d3b78bc7524b4080b785837f61a25e26a8
Christian Abhayaratna <chrisabh@gmail.com>
2018-10-24 12:31:33 +1100

    Progress

--- eckleburg.py
+++ eckleburg.py
@@ -1,4 +1,7 @@
 from bnWebsocket.klines import bnStream
+from bnWebsocket.languageHandled import languageHandler
+import slack
+import keychain
 import ccxt
 import time
 
@@ -7,33 +10,45 @@ import time
 
 class Eckleburg():
 	def __init__(self, pairs, timeFrame):
+		self.movement=0.025
 		self.TF=timeFrame
 		self.pairs=pairs
-		self.establishTime()
 		self.establishPrice()
-		bnStream(pairs, timeFrame,[self.TimeMangement])
+		bnStream(pairs, timeFrame,[self.Eye])
 
-	def establishTime(self):
-		print('GMT-0 Established')
-		self.gmt0=int(time.time()/86400)
 
 	def establishPrice(self):
+		'''Establish the previous day's open to comile into dictionary upon startup
+		To be used as a reference price for the price watch'''
+		self.reference={}
 		for pair in self.pairs:
-			tik=ccxt.binance().fetch_ohlcv(symbol=pair, timeframe=self.TF, since=(int(time.time()/86400)*86400-86401)*1000)
-			print(pair, tik)
-
-	def TimeMangement(self, klines):
-		if time.time()-self.gmt0*86400<(86400/2):
-			print(time.time()-self.gmt0*86400)
-			print('nothing')
-		else:
-			self.establishTime()
+			tik=ccxt.binance().fetch_ohlcv(symbol=pair, timeframe=self.TF, since=int(time.time()/86400)*86400*1000)
+			tik=tik[0][4]
+			self.reference.update({pair:tik})
+		print(self.reference)
 
 
-	def Printer(self, klines):
-		print(klines)
+	def Eye(self, klines):
+		market =languageHandler(output_lang = 'TradeModule',inputs =[klines.market],input_lang = "Binance")[0]
+		price = klines.kClose
+		print(f'{market}: {price}')
+		if price > self.reference[market]*(1+self.movement):
+			print(f'{market} {self.movement*100}% above reference price')
+			self.reference.update({market:price})
+			print(f'{market} reference price updated: {price}')
+
+		elif price < self.reference[market]*(1-self.movement):
+			#Send Bear Alert
+			print(f'{market} {self.movement*100}% below reference price')
+			self.reference.update({market:price})
+			print(f'{market} reference price updated: {price}')
+
+	def Hermes(self, msg):
 
 
+pairlist=['BTC/USDT',
+'ETH/USDT',
+'NEO/USDT',
+'MCO/BTC']
 
-
-Eckleburg(['BTC/USDT'], '1d')
+Eckleburg(pairlist, '1d')
